5eaa9a82b28ad67b4c31e3cbc38309c14c2d05dd
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-03-12 19:22:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ DB_NAME = os.environ.get("DB_NAME", "bharath")
 # Set the number of worker threads for the bot. To avoid sleeping issues and improve performance, keep it above 50 (recommended).
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
 # Message displayed to users when they click the Start button or send the /start command.
-START_MSG = os.environ.get("START_MESSAGE", "⚠️ This is a File Store Bot\n\nYou cannot search movies here.\n\n🎬 To download movies, go to our channel and check the channel Links.\n\n📢 Channel: https://t.me/Askmovies4")
+START_MSG = os.environ.get("START_MESSAGE", "⚠️ This is a File Store Bot\n\nYou cannot search movies here.\n\n🎬 To download movies, go to our channel and check the channel Links.\n\n📢 Channel: https://t.me/indian_tha")
 USER_REPLY_TEXT = os.environ.get(
     "USER_REPLY_TEXT",
     "⚠️ This is a File Store Bot\n\n"
